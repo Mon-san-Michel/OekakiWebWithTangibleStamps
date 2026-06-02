7c985ee57051ce721de55d381f1969f125e40c56
Matsu-Mon <79431569+Mon-san-Michel@users.noreply.github.com>
2026-06-02 17:15:24 +0900

    debug20260602-2

--- js/scanner.js
+++ js/scanner.js
@@ -394,19 +394,19 @@ class PaintScanner extends Scanner{
         super.clickAction(event);
 
         switch(this.markerId){
-            case 0 : selected_tool = "pen"; break;
+            //case 0 : selected_tool = "pen"; break;
             case 2 : selected_tool = "circle"; break;
             case 4 : selected_tool = "triangle"; break;
             case 9 : selected_tool = "square"; break;
             case 13 : selected_tool = "starshape"; break;
         }
-        if(this.markerId == 0){
+        /*if(this.markerId == 0){
             this.painter._ToolChangeToPen();
             document.getElementById("text_result").textContent = selected_tool + " is active.";
-        } else {
-            this.painter._ToolChangeToStamp();
+        } else {*/
+            //this.painter._ToolChangeToStamp();
             this.painter.paintStamp(this.posX, this.posY, this.degrees, selected_tool);
             document.getElementById("text_result").textContent = "Stamp of " + selected_tool + " is painted at (" + this.posX + "," + this.posY + ").";
-        }
+        /*}*/
     }
 }
