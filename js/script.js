14406110c8dd7f0c3e9891348f88927755d456a5
Matsu-Mon <79431569+Mon-san-Michel@users.noreply.github.com>
2026-06-16 14:59:53 +0900

    syusei3_20260616

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ class App{
             posX: "0px", //body-tag's margin is 8px.
             posY: "0px", //body-tag's margin is 8px + div-tags' height 60 * 3.
             width: "720px", //比率は2480×3508に近くなるようにしたい
-            height: "2400px",
+            height: "240px",
             // bgColor: "#f5720055",
             // bgImage: "url(../images/bgScanner.png)",
             // bgActive: "url(../images/bgActive.png)",
